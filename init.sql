c3e0d178ae343ce3eea603192628d280f425fa75
Florian Hotze <dev@florianhotze.com>
2025-02-12 17:36:13 +0100

    Update SQL init script
    Related to https://github.com/llamara-ai/llamara-backend/pull/61.
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- init.sql
+++ init.sql
@@ -1,20 +1,21 @@
 
 create table authenticated_users (
-                                     username varchar(255) not null,
                                      displayName varchar(255),
+                                     username varchar(255) not null,
                                      primary key (username)
 );
 
 create table knowledge (
+                           token_count integer,
+                           created_at timestamp(6) with time zone not null,
+                           last_updated_at timestamp(6) with time zone not null,
                            id uuid not null,
                            checksum varchar(255) not null,
                            content_type varchar(255) not null,
-                           created_at timestamp(6) with time zone not null,
                            ingestion_status varchar(255) not null check (ingestion_status in ('PENDING','SUCCEEDED','FAILED')),
                            label varchar(255),
-                           last_updated_at timestamp(6) with time zone not null,
-                           source bytea not null,
                            type varchar(255) not null check (type in ('FILE','WEBLINK')),
+                           source bytea not null,
                            primary key (id)
 );
 
@@ -31,8 +32,8 @@ create table knowledge_tags (
 );
 
 create table sessions (
-                          id uuid not null,
                           created_at timestamp(6) with time zone not null,
+                          id uuid not null,
                           label varchar(255),
                           username varchar(255),
                           primary key (id)
